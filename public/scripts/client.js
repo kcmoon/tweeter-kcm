781524b07ea07eb9e54c8cbbae102390a427b311
Kenny Moon <kennymoon90@gmail.com>
2022-06-22 16:26:40 -0700

    Fixed broken css styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,60 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const createTweetElement = function(tweetData) {
+    const tweetHTML = `<article class="tweet">
+    <header class="tweet-container-header">
+      <div id="header-div">
+        <img id="user-image" src="${tweetData.user.avatars}">
+        <p name="user" id="user">${tweetData.user.name}</p>
+      </div>
+      <p name="handle" id="handle">${tweetData.user.handle}</p>
+    </header>
+    <div class="tweet-content">${tweetData.content.text}</div>
+    <footer>
+      <p name="posted-time" id="posted-time">${tweetData.created_at}</p>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+      </footer>
+    </article>`
+    
+    return tweetHTML;
+  }
 
+  const tweetsData = [
+  {"user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1655674535398
+  }
+]
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet);
+    }
+  };
+
+  renderTweets(tweetsData);
+});

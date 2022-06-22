332d8d2a323c5b84ea7c838efd841b1c155c803c
Kenny Moon <kennymoon90@gmail.com>
2022-06-21 22:12:42 -0700

    Created and filled tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
+// This jquery function tracks the characters of new tweets and warns the author of reaching the limit.
 $(document).ready(function() {
   $('#tweet-text').on('input', function() {
-    // let counter = $('.counter') <= this traverses the whole document !! AVOID IF POSSIBLE
+    // access the counter to allow it to be manipulated
     const counter = $(this).next().find('output.counter');
+    // tracks the character count of the new tweet
     const inputVal = $(this).val();
 
     counter.text(140-inputVal.length);
    
+    // adds removes class that changes color of counter
     if (inputVal.length > 140) {
       counter.addClass("negative-counter")
     }

be963d45694bf6aae8ed23b225db3045bca0b43e
Kenny Moon <kennymoon90@gmail.com>
2022-06-22 22:23:57 -0700

    Protected the app by using an escape method for any XSS attacks

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,18 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+
 $(document).ready(function() {
   // creates a new tweet article 
   const createTweetElement = function(tweetData) {
+    // function to prevent XSS
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+
     const tweetHTML = `<article class="tweet">
     <header class="tweet-container-header">
       <div id="header-div">
@@ -14,9 +23,9 @@ $(document).ready(function() {
       </div>
       <p name="handle" id="handle">${tweetData.user.handle}</p>
     </header>
-    <div class="tweet-content">${tweetData.content.text}</div>
+    <div class="tweet-content">${escape(tweetData.content.text)}</div>
     <footer>
-      <p name="posted-time" id="posted-time">${tweetData.created_at}</p>
+      <p name="posted-time" id="posted-time">${timeago.format(tweetData.created_at)}</p>
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -33,19 +42,26 @@ $(document).ready(function() {
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
+      $('#tweet-data').prepend($tweet);
     }
   };
 
   // stops the submit button from refreshing the page and saves data
-  $( "#post-tweet" ).submit(function( event ) {
-    console.log($( "#post-tweet" ).serialize())
+  $( "#post-tweet" ).submit(async function( event ) {
     event.preventDefault();
-    $.post( "/tweets", $( "#post-tweet" ).serialize() );
+    if ($('#tweet-text').val() === '') {
+      return alert('Invalid Post: Post cannot be empty. Please Try Again');
+    }
+    if ($('#tweet-text').val().length > 140) {
+      return alert('Invalid Post: Character Limit Exceeded. Please Try Again');
+    }
+    await $.post( "/tweets", $( "#post-tweet" ).serialize() );
+    loadtweets();
+    $('#tweet-text').val('');
   });
 
-  const loadtweets = function() {
-    $.ajax('/tweets', { method: 'GET' })
+  const loadtweets = async function() {
+    await $.ajax('/tweets', { method: 'GET' })
     .then(function (data) {
       console.log(data);
       renderTweets(data);
